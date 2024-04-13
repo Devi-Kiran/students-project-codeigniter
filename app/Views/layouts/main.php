5b91bab1b88f39ea220e44208b5313c5af6dc4eb
Devi-Kiran <devikiran333007@gmail.com>
2024-04-13 13:59:49 +0530

    updated

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -8,16 +8,38 @@
     <title>
         <?= esc($title) ?? 'CodeIgniter 4 Layout' ?>
     </title>
+    <style>
+        .remove-border {
+            border: 0px !important;
+        }
+        .side-nav-btn {
+            border: 0px !important;
+            outline: 0px !important;
+            background: #2878b5 !important;
+            color: white !important;
+            border-radius: 0px !important;
+            margin-bottom: 0px !important;
+        }
+        .side-nav-btn:hover {
+            border: 0px !important;
+            outline: 0px !important;
+        }
+        .side-nav-link {
+            border: 0px;
+            background: #00669A !important;
+            border-radius: 0px !important;
+            color: white !important;
+        }
+    </style>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header class="mb-4">
+    <header style="padding-left: 15%;" class="mb-4 fixed-top">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="/ametecs-students-project/public/dashboard">Ametecs</a>
+            <div class="container mx-0">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -40,10 +62,10 @@
             </div>
         </nav>
     </header>
-    <section class="container">
-        <div class="row">
-            <?= $this->include("widgets/side_navbar")?>
-            <div class="col-9">
+    <section style="margin-top: 70px;">
+        <?= $this->include("widgets/side_navbar")?>
+        <div style="padding-left: 15%;">
+            <div class="px-4">
                 <?= $this->renderSection('content')?>
             </div>
         <div>
--- app/Views/widgets/side_navbar.php
+++ app/Views/widgets/side_navbar.php
@@ -1,11 +1,117 @@
-<div class="col-3">
-    <div class="list-group">
-      <button type="button" class="list-group-item list-group-item-action active">
-        Link 1
-      </button>
+<div class="fixed-top fixed-bottom overflow-scroll" style="width: 15%; background-color: #2878b5;">
+    <div class="my-1 text-center">
+        <a href="/ametecs-students-project/public/dashboard">
+            <img src="https://dcm.ametecsindia.com/assets/uploads/slam_images/ametecs_light.png" />
+        </a>
+    </div>
+    <div id="accordion">
+        <div class="remove-border card">
+            <button type="button" class="side-nav-btn list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                Dashboard
+            </button>
 
-      <?php for($i = 2; $i <= 10; $i++): ?>
-        <button type="button" class="list-group-item list-group-item-action">Link <?= $i?></button>
-      <?php endfor; ?>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Dashboard
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Analysis
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Search AC
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Search CR
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Dialer
+                </a>
+            </div> 
+        </div>
+
+        <div class="remove-border card">
+            <button type="button" class="side-nav-btn list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Allocations
+            </button>
+        
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Allocations
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Analysis
+                </a>
+            </div>
+        </div>
+
+        <div class="remove-border card">
+            <button type="button" class="side-nav-btn list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Smart Rewards
+            </button>
+        
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Allocations
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Analysis
+                </a>
+            </div>
+        </div>
+
+        <div class="remove-border card">
+            <button type="button" class="side-nav-btn list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                Assessment
+            </button>
+        
+            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Allocations
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Analysis
+                </a>
+            </div>
+        </div>
+
+        <div class="remove-border card">
+            <button type="button" class="side-nav-btn list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                Milestone
+            </button>
+        
+            <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Allocations
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Analysis
+                </a>
+            </div>
+        </div>
+
+        <div class="remove-border card">
+            <button type="button" class="side-nav-btn list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                Dashboard
+            </button>
+
+            <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Dashboard
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Analysis
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Search AC
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Search CR
+                </a>
+                <a href="#" class="side-nav-link list-group-item list-group-item-action">
+                    Dialer
+                </a>
+            </div> 
+        </div>
+    
     </div>
 </div>
